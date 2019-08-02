b41d1eb5f598efb3219fa56e20651fc109d37300
Joseph Daniel Winters <jdwinters96@gmail.com>
2019-08-01 21:47:25 -0400

    Fix overly long lines in Bliksem

--- bliksem/sketch.js
+++ bliksem/sketch.js
@@ -36,13 +36,18 @@ class Bolt {
 function generateBolt(initialAngle, initialRadius, isRoot, momentum=0) {
 	let newBolt = new Bolt(initialAngle, initialRadius, isRoot, momentum);
 	let bolts = [];
-	while (newBolt.lastPoint[1] < max(DEFAULT_CANVAS_HEIGHT, DEFAULT_CANVAS_WIDTH) / 2) {
+	while (
+		newBolt.lastPoint[1]
+		< max(DEFAULT_CANVAS_HEIGHT, DEFAULT_CANVAS_WIDTH) / 2
+	) {
 		let newAngle =
 			newBolt.lastPoint[0]
 			+ random(newBolt.momentum - 0.1, newBolt.momentum + 0.1);
 		let newRadius = newBolt.lastPoint[1] + 10;
 		newBolt.addPoint(newAngle, newRadius);
-		if (newBolt.numBranches < newBolt.points.length / 12 && random() < 0.06) {
+		if (
+			newBolt.numBranches < newBolt.points.length / 12 && random() < 0.06
+		) {
 			bolts = bolts.concat(
 				generateBolt(newAngle, newRadius, false, -newBolt.momentum)
 			);
@@ -64,12 +69,18 @@ function drawBolts(centreX, centreY, numBolts) {
 			let currentBolt = bolts[k];
 			for (let i = 0; i < currentBolt.points.length - 1; ++i) {
 				strokeWeight(GRADIENT_ARRAY[j][0]);
-				stroke(lerpColor(background_colour, lightning_colour, GRADIENT_ARRAY[j][1]));
+				stroke(lerpColor(
+					background_colour, lightning_colour, GRADIENT_ARRAY[j][1]
+				));
 				line(
-					cos(currentBolt.points[i][0]) * currentBolt.points[i][1] + centreX,
-					sin(currentBolt.points[i][0]) * currentBolt.points[i][1] + centreY,
-					cos(currentBolt.points[i+1][0]) * currentBolt.points[i+1][1] + centreX,
-					sin(currentBolt.points[i+1][0]) * currentBolt.points[i+1][1] + centreY
+					cos(currentBolt.points[i][0]) * currentBolt.points[i][1]
+					+ centreX,
+					sin(currentBolt.points[i][0]) * currentBolt.points[i][1]
+					+ centreY,
+					cos(currentBolt.points[i+1][0]) * currentBolt.points[i+1][1]
+					+ centreX,
+					sin(currentBolt.points[i+1][0]) * currentBolt.points[i+1][1]
+					+ centreY
 				);
 			}
 		}
