4ab2f40e1de250fd07041cf99de61f89ec3da150
Joseph Daniel Winters <jdwinters96@gmail.com>
2019-08-01 21:44:35 -0400

    Refactor bolt drawing logic into separate method

--- bliksem/sketch.js
+++ bliksem/sketch.js
@@ -1,5 +1,8 @@
 const DEFAULT_CANVAS_WIDTH = 1200;
 const DEFAULT_CANVAS_HEIGHT = 800;
+let GRADIENT_ARRAY = [
+	[20, 0.1], [12, 0.2], [7, 0.3], [5, 0.4], [4, 0.6], [3, 0.8], [1, 1]
+];
 
 let lightning_colour = null;
 let background_colour = null;
@@ -34,17 +37,43 @@ function generateBolt(initialAngle, initialRadius, isRoot, momentum=0) {
 	let newBolt = new Bolt(initialAngle, initialRadius, isRoot, momentum);
 	let bolts = [];
 	while (newBolt.lastPoint[1] < max(DEFAULT_CANVAS_HEIGHT, DEFAULT_CANVAS_WIDTH) / 2) {
-		let newAngle = newBolt.lastPoint[0] + random(newBolt.momentum - 0.1, newBolt.momentum + 0.1);
+		let newAngle =
+			newBolt.lastPoint[0]
+			+ random(newBolt.momentum - 0.1, newBolt.momentum + 0.1);
 		let newRadius = newBolt.lastPoint[1] + 10;
 		newBolt.addPoint(newAngle, newRadius);
 		if (newBolt.numBranches < newBolt.points.length / 12 && random() < 0.06) {
-			bolts = bolts.concat(generateBolt(newAngle, newRadius, false, -newBolt.momentum));
+			bolts = bolts.concat(
+				generateBolt(newAngle, newRadius, false, -newBolt.momentum)
+			);
 			newBolt.numBranches += 1;
 		}
 		if (!isRoot && random() < 0.07) break;
 	}
 	bolts.push(newBolt);
 	return bolts;
+}
+
+function drawBolts(centreX, centreY, numBolts) {
+	let bolts = [];
+	for (let i = 0; i < 3; ++i) {
+		bolts = bolts.concat(generateBolt(random(0, 2 * PI), 0, true, 0));
+	}
+	for (let j = 0; j < GRADIENT_ARRAY.length; ++j) {
+		for (let k = 0; k < bolts.length; ++k) {
+			let currentBolt = bolts[k];
+			for (let i = 0; i < currentBolt.points.length - 1; ++i) {
+				strokeWeight(GRADIENT_ARRAY[j][0]);
+				stroke(lerpColor(background_colour, lightning_colour, GRADIENT_ARRAY[j][1]));
+				line(
+					cos(currentBolt.points[i][0]) * currentBolt.points[i][1] + centreX,
+					sin(currentBolt.points[i][0]) * currentBolt.points[i][1] + centreY,
+					cos(currentBolt.points[i+1][0]) * currentBolt.points[i+1][1] + centreX,
+					sin(currentBolt.points[i+1][0]) * currentBolt.points[i+1][1] + centreY
+				);
+			}
+		}
+	}
 }
 
 function setup() {
@@ -62,34 +91,5 @@ function setup() {
 	canvas.parent("mycanvas");
 	background(background_colour);
 
-	let bolts = [];
-	for (let i = 0; i < 3; ++i) {
-		bolts = bolts.concat(generateBolt(random(0, 2 * PI), 0, true, 0));
-	}
-
-	let centre = [DEFAULT_CANVAS_WIDTH / 2, DEFAULT_CANVAS_HEIGHT / 2];
-	let gradientArr = [
-		[20, 0.1],
-		[12, 0.2],
-		[7, 0.3],
-		[5, 0.4],
-		[4, 0.6],
-		[3, 0.8],
-		[1, 1]
-	];
-	for (let j = 0; j < gradientArr.length; ++j) {
-		for (let k = 0; k < bolts.length; ++k) {
-			let currentBolt = bolts[k];
-			for (let i = 0; i < currentBolt.points.length - 1; ++i) {
-				strokeWeight(gradientArr[j][0]);
-				stroke(lerpColor(background_colour, lightning_colour, gradientArr[j][1]));
-				line(
-					cos(currentBolt.points[i][0]) * currentBolt.points[i][1] + centre[0],
-					sin(currentBolt.points[i][0]) * currentBolt.points[i][1] + centre[1],
-					cos(currentBolt.points[i+1][0]) * currentBolt.points[i+1][1] + centre[0],
-					sin(currentBolt.points[i+1][0]) * currentBolt.points[i+1][1] + centre[1]
-				);
-			}
-		}
-	}
+	drawBolts(DEFAULT_CANVAS_WIDTH / 2, DEFAULT_CANVAS_HEIGHT / 2, 5)
 }
